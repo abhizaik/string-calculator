52efdf2fbedefc691fc365198233ccf93b4e8e00
abhizaik <abhizaik@gmail.com>
2024-12-04 23:23:16 +0530

    handled comma separated case

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,9 +2,19 @@ function stringCalculator(str) {
     if(str === ""){
         return 0;
     }
+
     if(str === "1"){
         return 1;
     }
+
+    // Handling comma separated case
+    let sum = 0
+    const str_array = str.split(',');
+    str_array.forEach(element => {
+        sum += parseInt(element);
+    });
+    return sum;
+
 }
 
 module.exports = stringCalculator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -4,9 +4,11 @@ const stringCalculator = require('./stringCalculator');
 // Test cases
 describe('stringCalculator function', () => {
 
-    test('should return the sum of string', () => {
+    test('should return the sum of comma separeted string', () => {
         expect(stringCalculator("")).toBe(0);
         expect(stringCalculator("1")).toBe(1);
+        expect(stringCalculator("1,5")).toBe(6);
+        expect(stringCalculator("1,5,10,20")).toBe(36);
     });
 
 });
