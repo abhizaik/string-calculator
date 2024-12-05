0deaff6e48ae179333ff685997c284b7c6f26b1c
abhizaik <abhizaik@gmail.com>
2024-12-05 21:56:53 +0530

    added code for custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,9 +3,17 @@ function stringCalculator(str) {
         return 0;
     }
 
-    // Handling comma and \n separated case
+    // Set delimiter
+    let delimiter = /[\n,]/; 
+    if (str.startsWith("//")) {
+        const parts = str.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        str = parts[1];
+    }
+
+    // Handling custom delimiter
     let sum = 0
-    const str_array = str.split(/[\n,]/);
+    const str_array = str.split(delimiter);
     str_array.forEach(element => {
         sum += parseInt(element);
     });
